443236f8786a3fd0444190a53d9ea4af784f66ed
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-24 20:07:25 -0300

    Implementação do login e permanência no chat

--- script.js
+++ script.js
@@ -6,14 +6,41 @@ functions
 
 calls? */
 
-const messagesList = document.querySelector("ul");
 
-const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-promise.then(processResponse);
+const messagesList = document.querySelector("ul");
+let object;
+
+askUsername();
+
+function askUsername() {
+    const username = prompt("Qual é o seu lindo nome?");
+    object = {name: username};
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", object);
+    promise.then(processResponseLogin);
+    promise.catch(processErrorLogin);
+}
+
+function processResponseLogin() {
+    enterChat();
+    
+    setInterval(function () {
+        axios.post("https://mock-api.driven.com.br/api/v6/uol/status", object);
+        
+    }, 5000);
+}
+
+function processErrorLogin() {
+    askUsername();
+}
+
+function enterChat() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(processResponse);
+}
 
 function processResponse(response) {
     const messages = response.data;
-    let time;
     let type;
 
 	for (let i = 0; i < messages.length; i++) {
@@ -24,8 +51,5 @@ function processResponse(response) {
         } else {
             messagesList.innerHTML += `<li class="${type}"><em>(${messages[i].time})</em> &nbsp<b>${messages[i].from}</b> ${type === "private_message" ? "&nbspreservadamente" : ""}&nbsppara&nbsp<b>${messages[i].to}</b>: ${messages[i].text}</li>`;
         }
-        
-        // simplification attempt
-        // messagesList.innerHTML += `<li class="${type}">(${messages[i].time}) ${messages[i].from} ${type === "status" ? "" : type === "private_message" ? `reservadamente para ${messages[i].to}` : `para ${messages[i].to}`} ${messages[i].text}</li>`;
     }
 }
