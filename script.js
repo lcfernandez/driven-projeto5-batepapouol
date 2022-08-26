0dd69e69cf0fc364c1e74ca88d3e455f9328d369
Luiz Cláudio <lcfernandez@cos.ufrj.br>
2022-08-26 15:20:10 -0300

    Rolagem automática para a última mensagem

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ function processRespondeUpdate(response) {
                 }
             }
 
+            messagesList.lastElementChild.scrollIntoView();
             messages = update;
             return;
         }
